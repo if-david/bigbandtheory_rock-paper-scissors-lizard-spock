95bf56e67c0987ac5a6d8eff9456c456c88db601
if-david <damb86@hotmail.com>
2023-05-12 15:22:59 -0600

    Second commit

--- rspls.py
+++ rspls.py
@@ -1,19 +1,25 @@
-# The key idea of this program is to equate the strings
-# "rock", "paper", "scissors", "lizard", "Spock" to comp_numbers
-# as follows:
-#
+# keys:
 # 0 - rock
 # 1 - Spock
 # 2 - paper
 # 3 - lizard
 # 4 - scissors
 
+# Scissors cuts paper. 
+# Paper covers rock. 
+# Rock crushes lizard. 
+# Lizard poisons Spock. 
+# Spock smashes scissors. 
+# Scissors decapitates lizard. 
+# Lizard eats paper. 
+# Paper disproves Spock. 
+# Spock vaporizes rock. 
+# Rock crushes scissors.
 
 # Libraries and variables
 import random
 import os
 os.system("cls")
-
 
 # compute random guess for comp_comp_number using random
 comp_number = random.randint(0, 4)
@@ -49,7 +55,7 @@ def player_choice_to_player_number(player_choice):
 
 # convert comp_comp_number to comp_choice using the function comp_number_to_comp_choice()
 def comp_number_to_comp_choice(comp_number):
-    # delete the following pass statement and fill in your code below
+    # convert comp_number to a comp_choice using if/elif/else
     if comp_number == 0:
         comp_choice = 'rock'
     elif comp_number == 1:
@@ -60,20 +66,23 @@ def comp_number_to_comp_choice(comp_number):
         comp_choice = 'lizard'
     elif comp_number == 4:
         comp_choice = 'scissors'
-    return comp_choice
-    
-    
-    # convert comp_number to a player_choice using if/elif/else
     # don't forget to return the result!
-    
+    return comp_choice
 
 def rpsls(player_choice): 
     # delete the following pass statement and fill in your code below
     player_number = player_choice_to_player_number(player_choice)
+    
     # print out the message for the player's choice
     print(f'Player chooses {player_choice}')
+    
+    # print out the message for computer's choice
     print(f'Computer chooses {comp_number_to_comp_choice(comp_number)}')
+    
+    # compute difference of comp_number and player_number using module five
     result = player_number - comp_number
+    
+    # use if/elif/else to determine winner, print winner message
     if result == 0:
         print('Player and computer tie!')
     if result < 0:
@@ -82,32 +91,10 @@ def rpsls(player_choice):
         print('Player wins!')
     # print a blank line to separate consecutive games
     print()
-        
-    
-    
-
-    
-
-    
-
-    
-
-    
-    
-    # print out the message for computer's choice
-
-    # compute difference of comp_comp_number and player_number modulo five
-
-    # use if/elif/else to determine winner, print winner message
-
-    
-# test your code - THESE CALLS MUST BE PRESENT IN YOUR SUBMITTED CODE
+       
+# test code with different calls
 rpsls('rock')
 rpsls('spock')
 rpsls('paper')
 rpsls('lizard')
 rpsls('scissors')
-
-# always remember to check your completed program against the grading rubric
-
-
